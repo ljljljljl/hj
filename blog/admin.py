2b329a35e9a2121399a3d0a00c2ab8ac2dcfba87
lj <2687378115@qq.com>
2021-06-02 21:44:32 +0800

    美化

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib  import admin
-from . import models
-# Register your models here.
-admin.site.register(models.Blog)
+from .models import Blog
+admin.site.register(Blog)
+
+
+
--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(default='标题',max_length=50)
     date = models.DateField()
-    image = models.ImageField(default='default.png',upload_to='images/')
+    image = models.ImageField(default='default.png', upload_to='images/')
     text = models.TextField(default='正文')
+    
     def __str__(self):
         return self.title
