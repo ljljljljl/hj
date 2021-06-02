cd05199964228fbb409a900837f5547198d2b3b4
lj <2687378115@qq.com>
2021-06-02 18:49:00 +0800

    加了图片

--- gallery/models.py
+++ gallery/models.py
@@ -4,4 +4,14 @@ from django.db import models
 
 
 class Gallery(models.Model):
-    description  = models.CharField( max_length=500)
+    description  = models.CharField(default='内容', max_length=500)
+    image= models.ImageField(default='default.png',upload_to='images/')
+
+    title = models.CharField(default='作品标题', max_length=100)
+    def __str__(self):
+        return self.title
+        
+
+
+
+
